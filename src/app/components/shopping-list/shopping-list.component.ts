8b96ae86f9860014042ebe1331f072c487f4dee5
Ed Ezekiel <ed@ubiquisoft.com>
2021-02-16 08:31:23 -0600

    Create Ingredient model

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../../models';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
 
   constructor() { }
 
