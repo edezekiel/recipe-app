f5e597c46ebb7bfa8a87a4c0fd96aabcf8d1264c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-05-17 15:50:40 -0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'recipes',
+    component: RecipeBookComponent
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
