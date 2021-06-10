81893ffae1db4ad3b179fbf5698ea2bc8230463d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-06-10 15:27:34 -0500

    Add ngModel to form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  isLoginMode = true;
+  
+  toggleIsLoginMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 
+  onSubmit(form: NgForm) {
+    const { email, password } = form.value;
+    console.log(email, password);
+
+    form.reset();
+  }
 }
