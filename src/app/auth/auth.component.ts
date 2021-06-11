f45a05df838a04e3dba42c9547601e5e8bc9c01e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-06-11 09:25:28 -0500

    Refactor auth form submit logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
@@ -23,35 +23,22 @@ export class AuthComponent {
     }
 
     const { email, password } = form.value;
-
+    let authObs: Observable<any>;
+    
     this.isLoading = true;
 
-    this.isLoginMode 
-      ? this._signIn(email, password)
-      : this._signUp(email, password);
+    authObs = this.isLoginMode 
+      ? this.authService.signIn(email, password)
+      : this.authService.signUp(email, password)
+ 
+    authObs.subscribe(userCredential => {
+        console.log(userCredential);
+        this.isLoading = false;
+      }, error => {
+        this.errorMessage = error.message;
+        this.isLoading = false;
+      });
 
     form.reset();
-  }
-
-  private _signUp(email: string, password: string) {
-    return this.authService.signUp(email, password)
-      .subscribe(userCredential => {
-        console.log(userCredential);
-        this.isLoading = false;
-      }, error => {
-        this.errorMessage = error.message;
-        this.isLoading = false;
-      });
-  }
-
-  private _signIn(email: string, password: string) {
-    return this.authService.signIn(email, password)
-      .subscribe(userCredential => {
-        console.log(userCredential);
-        this.isLoading = false;
-      }, error => {
-        this.errorMessage = error.message;
-        this.isLoading = false;
-      });
   }
 }
